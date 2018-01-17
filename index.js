e77fb9605545314d482cdc39c4d56eaf223de394
Collin Edwards <collinedwards1579@gmail.com>
2018-01-17 23:50:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var name = "Joe"
 var height = "74"
 
 
-// Don't worry about this
+// Don't worry about this -- Oh, I'm gonna!
 module.exports = { name, height
 }
 
+if (name === "Bungazar") {
+  console.log("All hail Bungazar!")
+} else {
+  console.log("You are no king of mine!")
+}
